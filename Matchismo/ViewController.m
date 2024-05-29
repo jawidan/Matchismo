22aa44c948bf3a8e31db3170ea828f0fc111170e
Javidan Ibrahimov <ibrahimovjavidan@gmail.com>
2024-05-29 14:35:47 +0300

    Revert "Merge pull request #1 from jawidan/flip-cards"
    This reverts commit 992313a0117405f8af9e1566143e6dede054c6af, reversing
    changes made to b8b9776bfdc9acce3d335eea295791dc157e893d.

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -5,28 +5,9 @@
 //  Created by Javidan Ibrahimov on 28.05.2024.
 //
 
-//
-//  ViewController.m
-//  Matchismo
-//
-//  Created by Javidan Ibrahimov on 28.05.2024.
-//
-
 #import "ViewController.h"
-#import "PlayingCardDeck.h"
-#import "PlayingCard.h"
 
 @interface ViewController ()
-
-@property (strong, nonatomic) PlayingCardDeck *deck;
-
-@property (nonatomic) BOOL isShowingBack;
-
-@property (nonatomic) NSInteger flipCount;
-
-@property (weak, nonatomic) IBOutlet UILabel *counterLabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *gameOverLabel;
 
 @end
 
@@ -34,52 +15,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.deck = [PlayingCardDeck new];
-    self.isShowingBack = YES;
-    self.flipCount = 0;
+    // Do any additional setup after loading the view.
 }
-
-- (IBAction)tappedButton:(UIButton *)sender {
-    
-    if (self.isShowingBack){
-        [self showCardFront:sender];
-    }
-    else {
-        [self showCardBack:sender];
-    }
-    
-    self.isShowingBack = !self.isShowingBack;
-    self.flipCount += 1;
-    
-    self.counterLabel.text = [NSString stringWithFormat:@"Number of times you flipped : %lu", self.flipCount];
-    
-}
-
-- (void)showCardFront:(UIButton *)button {
-        PlayingCard *randomCard = (PlayingCard *)[self.deck drawRandomCard];
-    if (randomCard != nil) {
-        NSString *cardTitle = [NSString stringWithFormat:@"%@%@", [PlayingCard rankStrings][randomCard.rank], randomCard.suit];
-        
-        [button setTitle:cardTitle forState:UIControlStateNormal];
-        [button setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
-        
-    }
-    else {
-        [self endTheGame:button];
-    }
-}
-
-- (void)showCardBack:(UIButton *)button {
-    [button setTitle:@"" forState:UIControlStateNormal];
-    [button setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
-}
-
-- (void)endTheGame:(UIButton *)button {
-    button.enabled = NO;
-    button.highlighted = NO;
-    self.gameOverLabel.hidden = NO;
-}
-
 
 
 @end
